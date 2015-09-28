519700637e682ea1e56535355c07184274a237fa
syaginuma <keshinkasubs@gmail.com>
2015-09-28 06:24:22 +0000

    add posting microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
    validates :body, length: { maximum: 400 }
-    has_secure_password
+   has_secure_password
+   has_many :microposts
 end
